edb60fcfb46106ccd249ca6a54a6ab42f21b5139
shaurya35 <shauryajha35@gmail.com>
2024-04-18 14:33:28 +0530

    create

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const Campground = require("./models/campground");
-const campground = require("./models/campground");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
   useNewUrlParser: true,
@@ -21,6 +20,9 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.urlencoded({ extended: true }));
+
+
 app.get("/", (req, res) => {
   res.render("home");
 });
@@ -31,9 +33,14 @@ app.get("/campgrounds", async (req, res) => {
 });
 
 
-app.get('/campgrounds/new', async (req,res) => {
-    const campground = await Campground.findById(req.params.id)
-    res.render('campgrounds/edit', { campground });
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+})
+
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
 })
 
 app.get('/campgrounds/:id', async (req, res,) => {
