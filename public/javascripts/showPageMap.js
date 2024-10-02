595f62891e518d1c5ad7a1aaab3822ce90017f74
Shaurya Jha <shauryajha35@gmail.com>
2024-10-02 13:43:08 +0530

    Productionization Changes

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,21 +1,21 @@
-mapboxgl.accessToken = mapToken;
-const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/light-v10', // stylesheet location
-    center: campground.geometry.coordinates, // starting position [lng, lat]
-    zoom: 10 // starting zoom
-});
+if (typeof mapToken !== 'undefined' && mapToken) {
+    mapboxgl.accessToken = mapToken;
+    const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/light-v10', // stylesheet location
+        center: campground.geometry.coordinates, // starting position [lng, lat]
+        zoom: 10 // starting zoom
+    });
 
-map.addControl(new mapboxgl.NavigationControl());
+    map.addControl(new mapboxgl.NavigationControl());
 
-
-new mapboxgl.Marker()
-    .setLngLat(campground.geometry.coordinates)
-    .setPopup(
-        new mapboxgl.Popup({ offset: 25 })
-            .setHTML(
-                `<h3>${campground.title}</h3><p>${campground.location}</p>`
-            )
-    )
-    .addTo(map)
-
+    new mapboxgl.Marker()
+        .setLngLat(campground.geometry.coordinates)
+        .setPopup(
+            new mapboxgl.Popup({ offset: 25 })
+                .setHTML(`<h3>${campground.title}</h3><p>${campground.location}</p>`)
+        )
+        .addTo(map);
+} else {
+    console.log("Mapbox token is missing, Mapbox features are disabled.");
+}
